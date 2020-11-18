21c13f37864029aa038c0b69b46617785d706df5
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-11-18 21:23:56 +0900

    [백준] 택시 기하학 문제 풀이

--- backjoon/TaxicabGeometry.java
+++ backjoon/TaxicabGeometry.java
@@ -3,6 +3,7 @@ package algorithmPractice;
 /*
  * 택시 기하학 
  * https://www.acmicpc.net/problem/3053
+ * 유클리드 기하학에서 원은 우리가 알고있는 원이지만, 택시 기하학에서는 정사각형을 나타냄(지름은 정사각형의 대각선) 
  */
 
 import java.io.BufferedReader;
