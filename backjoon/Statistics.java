715653d933b1f857745a4cf935cc9ed2ebe8ee60
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-11-21 00:00:10 +0900

    [백준] 통계학 문제 풀이

--- backjoon/Statistics.java
+++ backjoon/Statistics.java
@@ -5,26 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /*
-문제
-수를 처리하는 것은 통계학에서 상당히 중요한 일이다. 통계학에서 N개의 수를 대표하는 기본 통계값에는 다음과 같은 것들이 있다. 단, N은 홀수라고 가정하자.
-
-산술평균 : N개의 수들의 합을 N으로 나눈 값
-중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
-최빈값 : N개의 수들 중 가장 많이 나타나는 값
-범위 : N개의 수들 중 최댓값과 최솟값의 차이
-N개의 수가 주어졌을 때, 네 가지 기본 통계값을 구하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 수의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 그 다음 N개의 줄에는 정수들이 주어진다. 입력되는 정수의 절댓값은 4,000을 넘지 않는다.
-
-출력
-첫째 줄에는 산술평균을 출력한다. 소수점 이하 첫째 자리에서 반올림한 값을 출력한다.
-둘째 줄에는 중앙값을 출력한다.
-셋째 줄에는 최빈값을 출력한다. 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값을 출력한다.
-넷째 줄에는 범위를 출력한다.
+ * 백준 통계학
+ * https://www.acmicpc.net/problem/2108
  */
 
 public class Statistics {
@@ -34,8 +20,9 @@ public class Statistics {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		List<Integer> list = new ArrayList<>();
 		double average = 0; 
+		int r_average = 0;
 		int middle = 0;
-		int common = 0;
+		int common = 0, b_time = 0, t_time = 0, order = 0, temp = 0, before = 4001;
 		int range = 0;
 		int cnt = 0;
 		
@@ -47,16 +34,30 @@ public class Statistics {
 		}
 		Collections.sort(list);
 		
-		//산술 평균 Math.round(e*100)/100.0
-		average = Math.round((average / list.size()) * 100 / 100.0);
+		//산술 평균
+		r_average = (int) Math.round(average / list.size());
 		
 		//중앙값 
 		middle = list.get(list.size()/2);
 		
 		//범위 
 		range = list.get(list.size()-1) - list.get(0);	
+		
+		//최빈값 : 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값을 출력한다.
+		for(int i = 0; i < list.size(); i++) {
+			temp = list.get(i);
+			if(temp == before) t_time++;
+			else t_time = 1;
+			
+			if(t_time > b_time) {
+				b_time = t_time; 
+				common = temp;
+				order = 1;
+			}
+			else if(t_time == b_time) if(++order == 2) common = temp; 
+			
+			before = temp; 
+		}				
+		System.out.println(r_average +  "\n" + middle + "\n" + common + "\n" + range);
 	}
-
 }
-
-
