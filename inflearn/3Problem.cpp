d27f7facb52783578ef0bc984a560297e08c4e94
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-03-16 21:20:51 +0900

    진약수의 합

---
+++ inflearn/3Problem.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+using namespace std; 
+
+/*
+자연수 N이 주어지면 자연수 N의 진약수의 합을 수식과 함께 출력하는 프로그램을 작성하세요.
+*/
+
+int main(void) {
+    int n, sum=0; 
+
+    cin >> n;
+
+    // 1은 모든 수의 진약수에 포함되니까, 1을 무조건 포함하는 방법으로 패턴을 구현한다.
+    cout << 1;
+
+    for(int i=2; i<n; i++) {
+        if( n % i == 0) {
+            cout << " + " << i; 
+            sum += i;
+        }
+    }
+
+    cout << " = " << sum;
+
+    return 0;
+}
