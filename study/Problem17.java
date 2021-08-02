05c96a97c0e483b327dbf802f4c4b0dc1e8cdad6
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-02 21:18:18 +0900

    4-1 2XN 타일링

---
+++ study/Problem17.java
@@ -0,0 +1,21 @@
+package study;
+
+public class Problem17 {
+
+    public static void main(String[] args) {
+        Problem17 problem17 = new Problem17();
+        System.out.println(problem17.solution(150));
+    }
+
+    public int solution(int n) {
+        int[] cache = new int[n+1];
+        return draw(n, cache);
+    }
+
+    private int draw(int n, int[] cache) {
+        if(n <= 2) return n;
+        if(cache[n] > 0) return cache[n];
+
+        return cache[n] = (draw(n-1, cache) + draw(n-2, cache)) % 1000000007;
+    }
+}
