91a029ce758a957d14470e8d6aa42baf839629bd
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-04 11:14:52 +0900

    Problem18 체육복 DFS를 그리디로 변경해보자

---
+++ study/Problem19.java
@@ -0,0 +1,45 @@
+package study;
+
+import java.util.Arrays;
+
+/**
+ * Problem18을 그리디로 풀어봅시다.
+ */
+
+public class Problem19 {
+
+    public static void main(String[] args) {
+        Problem19 problem19 = new Problem19();
+        System.out.println(problem19.solution(5, new int[] {2, 4}, new int[] {1,3,5}));
+    }
+
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+        int[] cntOfClothes = new int[n+1];
+        Arrays.fill(cntOfClothes, 1);
+
+        for(int i=0; i<lost.length; i++) {
+            cntOfClothes[lost[i]]--;
+        }
+
+        for(int i=0; i<reserve.length; i++) {
+            cntOfClothes[reserve[i]]++;
+        }
+
+        for(int i=1; i<cntOfClothes.length; i++) {
+            if(cntOfClothes[i] == 2) {
+                if(i!=0 && cntOfClothes[i-1] == 0)
+                    cntOfClothes[i-1] = cntOfClothes[i] = 1;
+                else if(i != (cntOfClothes.length-1) && cntOfClothes[i+1] == 0)
+                    cntOfClothes[i+1] = cntOfClothes[i] = 1;
+            }
+        }
+
+        for(int i=1; i<cntOfClothes.length; i++) {
+            if(cntOfClothes[i] > 0) answer++;
+        }
+
+        return answer;
+    }
+}
