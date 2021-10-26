0470ff299856ae4573a228f91efae365c4388399
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-10-27 00:33:24 +0900

    프로그래머스 - 주식가격 (스택 활용)

--- programmers/Problem40.java
+++ programmers/Problem40.java
@@ -1,17 +1,28 @@
+/**
+https://programmers.co.kr/learn/courses/30/lessons/42584
+*/
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        for(int i=0; i<prices.length; i++) {
-            for(int j=i+1; j<prices.length; j++) {
-                answer[i]++;
-                if(prices[i] > prices[j]) {
-                    break;
-                }                
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<prices.length-1; i++) {
+            stack.push(i);
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i+1]) {
+                int target = stack.pop();
+                answer[target] = i-target+1;
             }
         }
-        
+
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i] == 0) {
+                answer[i] = answer.length - i - 1;
+            }
+        }
+
         return answer;
     }
 }
